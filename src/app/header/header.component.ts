51eb6c05bf467ca3f90d59347f94c4d3ae320580
Kasu Samuel <kasusamuel17@mail.com>
2024-02-19 15:57:22 +0000

    first commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
+})
+
+export class HeaderComponent{
+    
+    // const myButton: HTMLButtonElement | null = document.querySelector('button') as HTMLButtonElement;
+
+   
+
+
+
+}
